945a7813709c90ab1ccb387d8239114254f9fa92
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-09 18:14:17 +0300

    Finish 04_removeFromArray task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
-
+const removeFromArray2 = function(arr, ...args) {
+    return arr.filter((item) => !args.includes(item));
 };
 
+const removeFromArray3 = function(arr, ...args) {
+    for (const arg of args) {
+        for (let i = 0; i < arr.length; ++i) {
+            if (arg === arr[i]) arr.splice(i--, 1);
+        }
+    }
+
+
+    return arr;
+}
+
+const removeFromArray = function() {
+    const arr = arguments[0];
+    for (let i = 1; i < arguments.length; ++i) {
+        for (let j = 0; j < arr.length; ++j) {
+            if (arguments[i] === arr[j]) arr.splice(j--, 1);
+        }
+    }
+    return arr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
