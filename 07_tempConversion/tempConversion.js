0ab25bf96880b063c4d80c16d48497afdc0dc511
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-09 18:54:17 +0300

    Finish 07_tempConversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return Math.round(5 * (fahrenheitTemp - 32) / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return Math.round((celsiusTemp * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
