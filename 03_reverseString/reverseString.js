88bc0d0685790664c42e91bb5d18aa72797a8d26
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-09 16:55:53 +0300

    Finish 03_reverseString task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; --i) {
+        reversed += str[i];
+    }
+    return reversed;
 };
+
+function reverseString2(str) {
+    return str.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
