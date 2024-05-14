142c83aa67f8ec7ee68e0844320763881143026c
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-14 20:38:34 +0300

    Finish 12_findTheOldest task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const currentAge = !current.yearOfDeath
+      ? new Date().getFullYear() - current.yearOfBirth
+      : current.yearOfDeath - current.yearOfBirth;
+    if (!("name" in oldest) || oldest.age < currentAge) {
+      oldest["name"] = current.name;
+      oldest["age"] = currentAge;
+    }
+    return oldest;
+  }, {});
+};
 
+// ideal solution
+function getAge(birth, death) {
+  death ??= new Date().getFullYear();
+  return death - birth;
+}
+
+const findOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    return oldestAge >= currentAge ? oldest : current;
+  });
 };
 
 // Do not edit below this line
