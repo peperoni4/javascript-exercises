2644b51a67bfceeaa7a0da920c0dd5f8d789502e
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-14 19:39:21 +0300

    Finish 09_palindroms task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const filtered = str.toLowerCase().replace(/[^\w\s\']|_| /g, "");
 
+  for (let i = 0, j = filtered.length - 1; i < j; ++i, --j) {
+    if (filtered[i] != filtered[j]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
